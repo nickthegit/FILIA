39e4f8b85d8a152fdb27a502b0de0772ee4e445c
nickthegit <nick@rosielee.co.uk>
2020-11-21 14:02:25 +0000

    Update sunIllustration.vue

--- components/Illustrations/sunIllustration.vue
+++ components/Illustrations/sunIllustration.vue
@@ -6,8 +6,8 @@
     xmlns:xlink="http://www.w3.org/1999/xlink"
     x="0px"
     y="0px"
-    viewBox="0 0 789.4 792.6"
-    style="enable-background: new 0 0 789.4 792.6"
+    viewBox="0 0 790 786"
+    style="enable-background: new 0 0 790 786"
     xml:space="preserve"
   >
     <style type="text/css">
@@ -18,16 +18,16 @@
     <linearGradient
       id="SVGID_1_"
       gradientUnits="userSpaceOnUse"
-      x1="-4168.7891"
-      y1="-5471.1597"
-      x2="-4168.7891"
-      y2="-4698.48"
-      gradientTransform="matrix(-1 0 0 1 -3774.1074 5481.1108)"
+      x1="-4185.1543"
+      y1="-5474.4507"
+      x2="-4185.1543"
+      y2="-4701.771"
+      gradientTransform="matrix(-1 0 0 1 -3790.1543 5481.1108)"
     >
       <stop offset="0.1977" style="stop-color: #ffcc33" />
       <stop offset="0.8337" style="stop-color: #f45e39" />
     </linearGradient>
-    <circle class="st0" cx="394.7" cy="396.3" r="386.3" />
+    <circle class="st0" cx="395" cy="393" r="386.3" />
   </svg>
 </template>
 
